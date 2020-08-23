7a5258d26c252f84a9969d6192f3d14d4cc8c982
bushatos <66446858+bushatos@users.noreply.github.com>
2020-08-23 10:57:43 -0400

    Per McKenzie email request 8/20/2020

--- Scripts/Event/DUA_EXECUTE_DIGEPLAN_SCRIPTS.js
+++ Scripts/Event/DUA_EXECUTE_DIGEPLAN_SCRIPTS.js
@@ -21,19 +21,20 @@ var doPreCache = false;
 //db updated per business request 4-27-2020
 //if(/*publicUser &&*/ capIDString.indexOf("TMP") == -1) emailDocUploadNotification(docGroupArrayModule,docTypeArrayModule);
 
-for (dl in newDocModelArray) {
-logDebug("<font color='green'>*****Document Details*****</font>");
-logDebug("<font color='green'>DocName: " + newDocModelArray[dl]["docName"] + " - DocID: " + newDocModelArray[dl]["documentNo"] + "</font>");
-logDebug("<font color='green'>DocGroup / DocCategory: " + newDocModelArray[dl]["docGroup"] + " / " + newDocModelArray[dl]["docCategory"] + "</font>");
-logDebug("<font color='green'>DocStatus: " + newDocModelArray[dl]["docStatus"] + "</font>");
-logDebug("<font color='green'>DocCategoryByAction: " + newDocModelArray[dl]["categoryByAction"] + "</font>");
-logDebug("<font color='green'>FileUploadBy: " + newDocModelArray[dl]["fileUpLoadBy"] + "</font>");
-	if(exists(newDocModelArray[dl]["docGroup"],docGroupArrayModule) && exists(newDocModelArray[dl]["docCategory"],docTypeArrayModule)) doPreCache = true;   
-	if(newDocModelArray[dl]["categoryByAction"] == "RESUBMIT") {
-		doResubmitActions(newDocModelArray[dl],docGroupArrayModule,docTypeArrayModule,routingTask,routingResubmittalStatus,originalDocStatusOnResubmit,parentDocStatusOnResubmit,resubmitDocStatusOnResubmit);
+if (capIDString.indexOf("TMP") == -1) {
+	for (dl in newDocModelArray) {
+	logDebug("<font color='green'>*****Document Details*****</font>");
+	logDebug("<font color='green'>DocName: " + newDocModelArray[dl]["docName"] + " - DocID: " + newDocModelArray[dl]["documentNo"] + "</font>");
+	logDebug("<font color='green'>DocGroup / DocCategory: " + newDocModelArray[dl]["docGroup"] + " / " + newDocModelArray[dl]["docCategory"] + "</font>");
+	logDebug("<font color='green'>DocStatus: " + newDocModelArray[dl]["docStatus"] + "</font>");
+	logDebug("<font color='green'>DocCategoryByAction: " + newDocModelArray[dl]["categoryByAction"] + "</font>");
+	logDebug("<font color='green'>FileUploadBy: " + newDocModelArray[dl]["fileUpLoadBy"] + "</font>");
+		if(exists(newDocModelArray[dl]["docGroup"],docGroupArrayModule) && exists(newDocModelArray[dl]["docCategory"],docTypeArrayModule)) doPreCache = true;   
+		if(newDocModelArray[dl]["categoryByAction"] == "RESUBMIT") {
+			doResubmitActions(newDocModelArray[dl],docGroupArrayModule,docTypeArrayModule,routingTask,routingResubmittalStatus,originalDocStatusOnResubmit,parentDocStatusOnResubmit,resubmitDocStatusOnResubmit);
+		}
 	}
 }
-
 if(doPreCache) {
      var docPreCache = digEplanPreCache("chesterfield",capIDString);
 }
