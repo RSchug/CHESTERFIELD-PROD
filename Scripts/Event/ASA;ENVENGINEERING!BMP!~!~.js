861628b72594da807f8f3f1d2fa80c342b36f72c
bushatos <66446858+bushatos@users.noreply.github.com>
2021-02-23 12:45:13 -0500

    Update

--- Scripts/Event/ASA;ENVENGINEERING!BMP!~!~.js
+++ Scripts/Event/ASA;ENVENGINEERING!BMP!~!~.js
@@ -1,2 +1,8 @@
 //When BMP Record is submitted add a child Sureties Record.
-createChild("EnvEngineering","Sureties","NA","NA","");
+try {
+	if (!publicUser) {
+		createChild("EnvEngineering","Sureties","NA","NA","");
+	}
+} catch (err) {
+    logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
+}
