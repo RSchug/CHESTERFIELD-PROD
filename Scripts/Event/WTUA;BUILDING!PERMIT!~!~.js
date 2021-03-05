d1346d0d186a44232bc2d54dddeb8aad67e48118
bushatos <66446858+bushatos@users.noreply.github.com>
2021-03-05 13:12:33 -0500

    update per auto-email

--- Scripts/Event/WTUA;BUILDING!PERMIT!~!~.js
+++ Scripts/Event/WTUA;BUILDING!PERMIT!~!~.js
@@ -30,12 +30,125 @@ try {
 			}
 		}
 	}
-	
-	/* if ((wfTask == "Permit Issuance" && wfStatus == "Issued") {
-		//01-2021 moved the auto-email from the DigEplan scripts to here - this is not working...
-		//var ApprovedStatus = 'Issued'; var docGroupArrayModule = 'General';
-		//emailReviewCompleteNotification_BLD(ApprovedStatus, docGroupArrayModule);
-		
+	//Temp CO Dates
+	var tempcoexpdate = "Temp CO Expiration Date";
+	var tempcoexpdatenew = jsDateToASIDate(getTaskDueDate("Inspections"));
+	if (wfStatus == 'Temporary CO Issued' && appMatch("Building/Permit/Residential/NA")) {
+	addFee("TEMPCORES","CC-BLD-ADMIN","FINAL",1,"Y");
+	editAppSpecific(tempcoexpdate,tempcoexpdatenew);
+	}
+	if (wfStatus == 'Temporary CO Issued' && appMatch("Building/Permit/Commercial/NA")) {
+		addFee("TEMPCO","CC-BLD-ADMIN","FINAL",1,"Y");
+		editAppSpecific(tempcoexpdate,tempcoexpdatenew);
+	}
+	//Variables for the EE Inspector based on Parcel field "Inspection Dist" and Standard Choice 'InspectionAssignmentEnvEngineering'
+	//var ParcelInspectorEnvEng = AInfo["ParcelAttribute.InspectionDistrict"];
+	//var InspAssignment = lookup("InspectionAssignmentEnvEngineering", ParcelInspectorEnvEng);
+	if (wfTask == 'Review Distribution' && wfStatus == 'Routed for Review') {
+		if (isTaskActive("Environmental Engineering Review")) {
+			assignTask_CHESTERFIELD("Environmental Engineering Review", null, null, null, "EnvEngineering");
+		}
+	}
+	//Temporary Elevator Renewal Certificate Dates
+	var tempcertexpdate = "Temporary Certificate Expiration Date";
+	var tempcertexpdatenew = jsDateToASIDate(new Date(dateAdd(null, 30)));
+	if (wfStatus == 'Temporary Certificate Issued' && appMatch("Building/Permit/Elevator/Renewal")) {
+	editAppSpecific(tempcertexpdate,tempcertexpdatenew);
+	}
+	//Email if Payment Due 
+	if (wfTask == "Application Submittal" && wfStatus == "Payment Due"){
+		var emailSendFrom = '';
+		var emailSendTo = "";
+		var emailCC = "";
+		var emailTemplate = "WTUA_BLDG_PAYMENT_DUE";
+		var fileNames = [];
+		var emailParameters = aa.util.newHashtable();
+		getRecordParams4Notification(emailParameters);
+		getAPOParams4Notification(emailParameters);
+		var acaSite = lookup("ACA_CONFIGS", "ACA_SITE");
+		acaSite = acaSite.substr(0, acaSite.toUpperCase().indexOf("/ADMIN"));
+		//getACARecordParam4Notification(emailParameters,acaSite);
+		addParameter(emailParameters, "$$acaRecordUrl$$", getACARecordURL(acaSite));
+		addParameter(emailParameters, "$$wfComment$$", wfComment);
+		addParameter(emailParameters, "$$wfStatus$$", wfStatus);
+		addParameter(emailParameters, "$$ShortNotes$$", getShortNotes());
+		var applicantEmail = "";
+		var contObj = {};
+		contObj = getContactArray(capId);
+		if (typeof(contObj) == "object") {
+			for (co in contObj) {
+				if (contObj[co]["contactType"] == "Applicant" && contObj[co]["email"] != null)
+					applicantEmail += contObj[co]["email"] + ";";
+			}
+			addParameter(emailParameters, "$$applicantEmail$$", applicantEmail);
+		} else { logDebug("No contacts at all for " + capIDString); }
+		if (applicantEmail != "") {
+			sendNotification(emailSendFrom, emailSendTo, emailCC, emailTemplate, emailParameters, fileNames);
+		} else { logDebug("No applicants for " + capIDString); }
+	}
+	//03-2021 Auto-emails
+	if (matches(wfTask,"Application Submittal","Review Distribution") && wfStatus == "Additional Information Required") { 
+		var emailSendFrom = '';
+		var emailSendTo = "";
+		var emailCC = "";
+		var emailTemplate = "WTUA_CONTACT NOTIFICATION_ADDITIONAL_BLD";
+		var fileNames = [];
+		var emailParameters = aa.util.newHashtable();
+		getRecordParams4Notification(emailParameters);
+		getAPOParams4Notification(emailParameters);
+		var acaSite = lookup("ACA_CONFIGS", "ACA_SITE");
+		acaSite = acaSite.substr(0, acaSite.toUpperCase().indexOf("/ADMIN"));
+		//getACARecordParam4Notification(emailParameters,acaSite);
+		addParameter(emailParameters, "$$acaRecordUrl$$", getACARecordURL(acaSite));
+		addParameter(emailParameters, "$$wfComment$$", wfComment);
+		addParameter(emailParameters, "$$wfStatus$$", wfStatus);
+		addParameter(emailParameters, "$$ShortNotes$$", getShortNotes());
+		var applicantEmail = "";
+		var contObj = {};
+		contObj = getContactArray(capId);
+		if (typeof(contObj) == "object") {
+			for (co in contObj) {
+				if (contObj[co]["contactType"] == "Applicant" && contObj[co]["email"] != null)
+					applicantEmail += contObj[co]["email"] + ";";
+			}
+			addParameter(emailParameters, "$$applicantEmail$$", applicantEmail);
+		} else { logDebug("No contacts at all for " + capIDString); }
+		if (applicantEmail != "") {
+			sendNotification(emailSendFrom, emailSendTo, emailCC, emailTemplate, emailParameters, fileNames);
+		} else { logDebug("No applicants for " + capIDString); }
+	}
+	if (((appMatch('Building/Permit/Residential/NA') && wfTask == 'Structural Review') || (appMatch('Building/Permit/Residential/Multi-Family') &&  wfTask == 'Structural Review') ||
+		matches(wfTask,"Review Consolidation")) &&  wfStatus == "Corrections Required") { 
+		var emailSendFrom = '';
+		var emailSendTo = "";
+		var emailCC = "";
+		var emailTemplate = "WTUA_CONTACT NOTIFICATION_CORRECTION_BLD";
+		var fileNames = [];
+		var emailParameters = aa.util.newHashtable();
+		getRecordParams4Notification(emailParameters);
+		getAPOParams4Notification(emailParameters);
+		var acaSite = lookup("ACA_CONFIGS", "ACA_SITE");
+		acaSite = acaSite.substr(0, acaSite.toUpperCase().indexOf("/ADMIN"));
+		//getACARecordParam4Notification(emailParameters,acaSite);
+		addParameter(emailParameters, "$$acaRecordUrl$$", getACARecordURL(acaSite));
+		addParameter(emailParameters, "$$wfComment$$", wfComment);
+		addParameter(emailParameters, "$$wfStatus$$", wfStatus);
+		addParameter(emailParameters, "$$ShortNotes$$", getShortNotes());
+		var applicantEmail = "";
+		var contObj = {};
+		contObj = getContactArray(capId);
+		if (typeof(contObj) == "object") {
+			for (co in contObj) {
+				if (contObj[co]["contactType"] == "Applicant" && contObj[co]["email"] != null)
+					applicantEmail += contObj[co]["email"] + ";";
+			}
+			addParameter(emailParameters, "$$applicantEmail$$", applicantEmail);
+		} else { logDebug("No contacts at all for " + capIDString); }
+		if (applicantEmail != "") {
+			sendNotification(emailSendFrom, emailSendTo, emailCC, emailTemplate, emailParameters, fileNames);
+		} else { logDebug("No applicants for " + capIDString); }
+	}
+	if (wfTask == "Permit Issuance" && wfStatus == "Issued") { 
 		var emailSendFrom = '';
 		var emailSendTo = "";
 		var emailCC = "";
@@ -64,82 +177,7 @@ try {
 		if (applicantEmail != "") {
 			sendNotification(emailSendFrom, emailSendTo, emailCC, emailTemplate, emailParameters, fileNames);
 		} else { logDebug("No applicants for " + capIDString); }
-	}*/
-		
-	//Temp CO Dates
-	var tempcoexpdate = "Temp CO Expiration Date";
-	var tempcoexpdatenew = jsDateToASIDate(getTaskDueDate("Inspections"));
-	if (wfStatus == 'Temporary CO Issued' && appMatch("Building/Permit/Residential/NA")) {
-	addFee("TEMPCORES","CC-BLD-ADMIN","FINAL",1,"Y");
-	editAppSpecific(tempcoexpdate,tempcoexpdatenew);
-	}
-	if (wfStatus == 'Temporary CO Issued' && appMatch("Building/Permit/Commercial/NA")) {
-		addFee("TEMPCO","CC-BLD-ADMIN","FINAL",1,"Y");
-		editAppSpecific(tempcoexpdate,tempcoexpdatenew);
-	}
-	//Variables for the EE Inspector based on Parcel field "Inspection Dist" and Standard Choice 'InspectionAssignmentEnvEngineering'
-	//var ParcelInspectorEnvEng = AInfo["ParcelAttribute.InspectionDistrict"];
-	//var InspAssignment = lookup("InspectionAssignmentEnvEngineering", ParcelInspectorEnvEng);
-	if (wfTask == 'Review Distribution' && wfStatus == 'Routed for Review') {
-		if (isTaskActive("Environmental Engineering Review")) {
-			assignTask_CHESTERFIELD("Environmental Engineering Review", null, null, null, "EnvEngineering");
-		}
-	}
-	//Temporary Elevator Renewal Certificate Dates
-	var tempcertexpdate = "Temporary Certificate Expiration Date";
-	var tempcertexpdatenew = jsDateToASIDate(new Date(dateAdd(null, 30)));
-	if (wfStatus == 'Temporary Certificate Issued' && appMatch("Building/Permit/Elevator/Renewal")) {
-	editAppSpecific(tempcertexpdate,tempcertexpdatenew);
 	}
 } catch (err) {
 	logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
-}
-function emailReviewCompleteNotification_BLD(ApprovedStatus, docGroupArrayModule) {
-    showMessageDefault = showMessage;
-    //populate email notification parameters
-    var emailSendFrom = "";
-    var emailSendTo = "";
-    var emailCC = "";
-    var emailParameters = aa.util.newHashtable();
-    var fileNames = [];
-
-    getRecordParams4Notification(emailParameters);
-    getAPOParams4Notification(emailParameters);
-    var acaSite = lookup("ACA_CONFIGS", "ACA_SITE");
-    acaSite = acaSite.substr(0, acaSite.toUpperCase().indexOf("/ADMIN"));
-    //getACARecordParam4Notification(emailParameters,acaSite);
-    addParameter(emailParameters, "$$acaRecordUrl$$", getACARecordURL(acaSite));
-    addParameter(emailParameters, "$$wfComment$$", wfComment);
-    addParameter(emailParameters, "$$wfStatus$$", wfStatus);
-    addParameter(emailParameters, "$$ShortNotes$$", getShortNotes());
-
-    var applicantEmail = "";
-    var assignedTo = getAssignedToStaff();
-    var assignedToEmail = "";
-    var assignedToFullName = "";
-    var contObj = {};
-    contObj = getContactArray(capId);
-    if (typeof(contObj) == "object") {
-        for (co in contObj) {
-            if (contObj[co]["contactType"] == "Applicant" && contObj[co]["email"] != null)
-                applicantEmail += contObj[co]["email"] + ";";
-        }
-	    addParameter(emailParameters, "$$applicantEmail$$", applicantEmail);
-    }
-
-    if (applicantEmail != "") {
-        
-        if (exists(wfStatus, ApprovedStatus)) {
-			var emailTemplate = "WTUA_CONTACT NOTIFICATION_APPROVED_BLD";
-        }
-        sendNotification(emailSendFrom, emailSendTo, emailCC, emailTemplate, emailParameters, fileNames);
-    } /*else {
-        if (applicantEmail == "" && assignedToEmail != "") {
-            var emailTemplate = "WTUA_INTERNAL NOTIFICATION_REVIEWCOMPLETE";
-            sendNotification(emailSendFrom, emailSendTo, emailCC, emailTemplate, emailParameters, fileNames);
-            showMessage = true;
-            comment("There is no applicant email associated to this permit. Permit Coordinator has been notified via email to contact this applicant directly.");
-            showMessage = showMessageDefault;
-        }
-    } */
 }
