85ae305452146020df33de05eb63fe46c156e88c
SYSTEM <SYSTEM@mtprod-AA-2.us.accela.com>
2021-03-05 04:25:07 -0600

    commit by emse tools 2021-03-05 04:25:07

--- Scripts/Event/IRSA;BUILDING!~!~!~.js
+++ Scripts/Event/IRSA;BUILDING!~!~!~.js
@@ -3,13 +3,13 @@ try {
 	var inspBillable = inspObj.getInspection().getActivity().getInspBillable();
 	logDebug("Inspection Billable checkbox = " + inspBillable + ". And Inspection Result = " + inspResult);
 	if (inspBillable == "Y" && matches(inspResult,"Corrections Required","Approved")) {
-		addFeeWithExtraData("REINSPECTION","CC-BLD-ADMIN","FINAL",1,"Y",capId,inspType+" Insp.Seq# "+Math.round(inspTotalTime)+"");
+		addFeeWithExtraData("REINSPECTION","CC-BLD-ADMIN","FINAL",1,"N",capId,inspType+" Insp.Seq# "+Math.round(inspTotalTime)+"");
 	}
 	//52B:If Inspection Result = Corrections Required, and 'Not Ready Fee' is checked, then add Not Ready Fee("CC-BLD-ADMIN", "NOTREADY"). Inspection Detail Page field is called 'Overtime'
 	var inspOvertime = inspObj.getInspection().getActivity().getOvertime();
 	logDebug("Inspection Not Ready (Overtime) checkbox = " + inspOvertime + ". And Inspection Result = " + inspResult);
 	if (matches(inspResult, "Corrections Required") && inspOvertime == "Y") {
-		addFeeWithExtraData("NOTREADY","CC-BLD-ADMIN","FINAL",1,"Y",capId,inspType+" Insp.Seq# "+Math.round(inspTotalTime)+"");
+		addFeeWithExtraData("NOTREADY","CC-BLD-ADMIN","FINAL",1,"N",capId,inspType+" Insp.Seq# "+Math.round(inspTotalTime)+"");
 	}
 
 	//If Residential Building with Nature of Work of 'New Construction of Single Family Dwelling, Industrialized Building, Manufactured or Mobile Home on Private Property and Inspection Result is "Approved" for Inspection Type "Building Final" close the Inspections Workflow Task.//
@@ -35,7 +35,7 @@ try {
 		}
 		}
 	if (appMatch("Building/Permit/Commercial/NA") && inspType.equals("Building Final") && inspResult.equals("Approved")){
-		closeTask("Inspections","CO Ready to Issue","Updated based on Completed Inspection Result","");
+		closeTask("Inspections","Completed","Updated based on Completed Inspection Result","");
 		activateTask("Certificate Issuance");
 		}
 	if (appMatch("Building/Permit/Commercial/Demolition") && inspType.equals("Building Final") && inspResult.equals("Approved")){
