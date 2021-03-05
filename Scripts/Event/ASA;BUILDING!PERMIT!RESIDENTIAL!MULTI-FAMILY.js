5a99f4938d2a1216ebb15c4e1bd282573601ea7e
bushatos <66446858+bushatos@users.noreply.github.com>
2021-03-05 05:39:46 -0500

    remove dup scripts

--- Scripts/Event/ASA;BUILDING!PERMIT!RESIDENTIAL!MULTI-FAMILY.js
+++
@@ -1,7 +0,0 @@
-if (!publicUser) {
-	var NumberofUnits = AInfo["Number of Units"];
-
-	addFee("MULTIFAMILY","CC-BLD-MULTIFAMILY","FINAL",NumberofUnits,"Y");
-	addFee("STATELEVY","CC-BLD-MULTIFAMILY","FINAL",1,"Y");
-	addFee("EESITE","CC-BLD-MULTIFAMILY","FINAL",1,"Y");
-}
