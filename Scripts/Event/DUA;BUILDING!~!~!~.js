4000283251fbc04cc49be2cf9f630b68b1ca7a49
bushatos <66446858+bushatos@users.noreply.github.com>
2021-03-09 12:23:49 -0500

    per melissa update

--- Scripts/Event/DUA;BUILDING!~!~!~.js
+++ Scripts/Event/DUA;BUILDING!~!~!~.js
@@ -3,6 +3,7 @@ try {
 	if (publicUser && !matches(capStatus,'Submitted',null)) {
 		if (!isTaskActive("Document Submitted Online")) {
 				addAdHocTask("ADHOC_WF","Document Submitted Online","");
+				assignTask_CHESTERFIELD("Document Submitted Online", "MCLEHANEYD");
 		}		
     }
 //in DUA_EXECUTE_DIGEPLAN_SCRIPTS we update AppStatus when docs are uploaded by publicuser via the Resubmit button...
