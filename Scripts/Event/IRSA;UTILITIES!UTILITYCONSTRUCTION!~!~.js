ff7d747e0e426e256018658b83a1c32e71dc046e
SYSTEM <SYSTEM@mtprod-AA-0.us.accela.com>
2021-03-16 17:01:39 -0500

    commit by emse tools 2021-03-16 17:01:39

--- Scripts/Event/IRSA;UTILITIES!UTILITYCONSTRUCTION!~!~.js
+++ Scripts/Event/IRSA;UTILITIES!UTILITYCONSTRUCTION!~!~.js
@@ -1,8 +1,11 @@
-// IRSA: Utilities/UtilityConstruction/*/*
 // 14U When 'Daily Status Report' Inspection Type is updated to Status of 'Rejected' reassign Inspector to original Inspector
-if (inspType == "Daily Status Report" && matches(inspResult, "Rejected")) {
     var firstInspectorID = getFirstInspector(inspType);
+    var ProjectComplete = inspObj.getInspection().getActivity().getOvertime();
+if (inspType == "Daily Status Report" && matches(inspResult, "Rejected")) {
     assignInspection(inspId, firstInspectorID);
-    // scheduleInspectDate(inspType, dateAdd(null, 1, true), firstInspectorID, null, "Auto Scheduled"); // Auto Schedule for next working day.
-    // scheduleInspection(inspType, 1, firstInspectorID, null, "Auto Scheduled"); // Auto Schedule for next day.
+}
+//Add another Daily Status Report when Project Complete is not Y
+if (inspType == "Daily Status Report" && matches(inspResult, "Complete","Approved") && (ProjectComplete != "Y")) {
+	var inspcomment = getLastInspectioncomment(inspType);
+    scheduleInspection("Daily Status Report",1,firstInspectorID, null,inspcomment);
 }
