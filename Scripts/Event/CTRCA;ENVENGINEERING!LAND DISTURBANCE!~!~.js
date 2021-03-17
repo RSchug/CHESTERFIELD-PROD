c754bbf494500a32bbfa816b01ff863b14a3a55b
bushatos <66446858+bushatos@users.noreply.github.com>
2021-03-17 11:05:34 -0400

    update per johnny 2

---
+++ Scripts/Event/CTRCA;ENVENGINEERING!LAND DISTURBANCE!~!~.js
@@ -0,0 +1,8 @@
+//When Final Plat Record is submitted add a child Sureties Record.
+try {
+	if (publicUser) {
+		createChildLic("EnvEngineering","Sureties","ESC","NA","");
+	}
+} catch (err) {
+    logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
+}
