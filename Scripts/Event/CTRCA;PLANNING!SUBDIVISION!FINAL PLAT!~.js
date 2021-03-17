0ebe6570d848a5e1ed96357f6e1feebc56faec16
bushatos <66446858+bushatos@users.noreply.github.com>
2021-03-17 11:04:09 -0400

    update per Johnny

--- Scripts/Event/CTRCA;PLANNING!SUBDIVISION!FINAL PLAT!~.js
+++ Scripts/Event/CTRCA;PLANNING!SUBDIVISION!FINAL PLAT!~.js
@@ -1,7 +1,7 @@
 //When Final Plat Record is submitted add a child Sureties Record.
 try {
 	if (publicUser) {
-		createChild("EnvEngineering","Sureties","Performance","NA","");
+		createChildLic("EnvEngineering","Sureties","Performance","NA","");
 	}
 } catch (err) {
     logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
