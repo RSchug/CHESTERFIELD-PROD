3e27123687a421e7fe1f6dab68316986caf7f1e7
bushatos <66446858+bushatos@users.noreply.github.com>
2021-03-17 07:33:12 -0400

    Clean up on Johnny scripts

--- Scripts/Event/ASA;ENVENGINEERING!LAND DISTURBANCE!~!~.js
+++ Scripts/Event/ASA;ENVENGINEERING!LAND DISTURBANCE!~!~.js
@@ -1,7 +1,7 @@
 //When Land Disturbance Record is submitted add a child Sureties Record.
 try {
 	if (!publicUser) {
-		var newCapId = createChildLic("EnvEngineering","Sureties","ESC","NA",""); 
+		createChildLic("EnvEngineering","Sureties","ESC","NA",""); 
 	}
 } catch (err) {
     logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
