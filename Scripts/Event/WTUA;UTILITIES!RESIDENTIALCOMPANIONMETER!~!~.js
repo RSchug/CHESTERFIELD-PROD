592b2020f6217d26d8cf9524eb9effdee6fe4deb
SYSTEM <SYSTEM@mtprod-AA-2.us.accela.com>
2021-03-22 04:13:25 -0500

    commit by emse tools 2021-03-22 04:13:25

--- Scripts/Event/WTUA;UTILITIES!RESIDENTIALCOMPANIONMETER!~!~.js
+++ Scripts/Event/WTUA;UTILITIES!RESIDENTIALCOMPANIONMETER!~!~.js
@@ -1,10 +1,72 @@
 try {
-//Email if Payment Due 
+//Email if Additional Infor Required
 	if (wfTask == "Application Submittal" && wfStatus == "Additional Information Required"){
 		var emailSendFrom = '';
 		var emailSendTo = "";
 		var emailCC = "";
 		var emailTemplate = "WTUA_UTIL_PAYMENT_DUE";
+		var fileNames = [];
+		var emailParameters = aa.util.newHashtable();
+		getRecordParams4Notification(emailParameters);
+		getAPOParams4Notification(emailParameters);
+		var acaSite = lookup("ACA_CONFIGS", "ACA_SITE");
+		acaSite = acaSite.substr(0, acaSite.toUpperCase().indexOf("/ADMIN"));
+		//getACARecordParam4Notification(emailParameters,acaSite);
+		addParameter(emailParameters, "$$acaRecordUrl$$", getACARecordURL(acaSite));
+		addParameter(emailParameters, "$$wfComment$$", wfComment);
+		addParameter(emailParameters, "$$wfStatus$$", wfStatus);
+		addParameter(emailParameters, "$$ShortNotes$$", getShortNotes());
+		var applicantEmail = "";
+		var contObj = {};
+		contObj = getContactArray(capId);
+		if (typeof(contObj) == "object") {
+			for (co in contObj) {
+				if (contObj[co]["contactType"] == "Applicant" && contObj[co]["email"] != null || contObj[co]["contactType"] == "Billing Contact" && contObj[co]["email"] != null)
+					applicantEmail += contObj[co]["email"] + ";";
+			}
+			addParameter(emailParameters, "$$applicantEmail$$", applicantEmail);
+		} else { logDebug("No contacts at all for " + capIDString); }
+		if (applicantEmail != "") {
+			sendNotification(emailSendFrom, emailSendTo, emailCC, emailTemplate, emailParameters, fileNames);
+		} else { logDebug("No applicants for " + capIDString); }
+	}
+//Email if Pending Domestic Acct Payment  
+	if (wfTask == "Application Submittal" && wfStatus == "Pending Domestic Acct Payment"){
+		var emailSendFrom = '';
+		var emailSendTo = "";
+		var emailCC = "";
+		var emailTemplate = "WTUA_UTIL_PENDING_DOMESTIC_PAYMENT";
+		var fileNames = [];
+		var emailParameters = aa.util.newHashtable();
+		getRecordParams4Notification(emailParameters);
+		getAPOParams4Notification(emailParameters);
+		var acaSite = lookup("ACA_CONFIGS", "ACA_SITE");
+		acaSite = acaSite.substr(0, acaSite.toUpperCase().indexOf("/ADMIN"));
+		//getACARecordParam4Notification(emailParameters,acaSite);
+		addParameter(emailParameters, "$$acaRecordUrl$$", getACARecordURL(acaSite));
+		addParameter(emailParameters, "$$wfComment$$", wfComment);
+		addParameter(emailParameters, "$$wfStatus$$", wfStatus);
+		addParameter(emailParameters, "$$ShortNotes$$", getShortNotes());
+		var applicantEmail = "";
+		var contObj = {};
+		contObj = getContactArray(capId);
+		if (typeof(contObj) == "object") {
+			for (co in contObj) {
+				if (contObj[co]["contactType"] == "Applicant" && contObj[co]["email"] != null || contObj[co]["contactType"] == "Billing Contact" && contObj[co]["email"] != null)
+					applicantEmail += contObj[co]["email"] + ";";
+			}
+			addParameter(emailParameters, "$$applicantEmail$$", applicantEmail);
+		} else { logDebug("No contacts at all for " + capIDString); }
+		if (applicantEmail != "") {
+			sendNotification(emailSendFrom, emailSendTo, emailCC, emailTemplate, emailParameters, fileNames);
+		} else { logDebug("No applicants for " + capIDString); }
+	}
+//Email if Pending Full Payment
+	if (wfTask == "Application Submittal" && wfStatus == "Pending Full Payment"){
+		var emailSendFrom = '';
+		var emailSendTo = "";
+		var emailCC = "";
+		var emailTemplate = "WTUA_UTIL_PENDING_FULL_PAYMENT";
 		var fileNames = [];
 		var emailParameters = aa.util.newHashtable();
 		getRecordParams4Notification(emailParameters);
