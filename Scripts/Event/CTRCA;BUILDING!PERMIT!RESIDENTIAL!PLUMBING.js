dff49699741bfa016923c15411303e0c4edba451
SYSTEM <SYSTEM@mtprod-AA-2.us.accela.com>
2021-03-22 04:11:50 -0500

    commit by emse tools 2021-03-22 04:11:50

--- Scripts/Event/CTRCA;BUILDING!PERMIT!RESIDENTIAL!PLUMBING.js
+++ Scripts/Event/CTRCA;BUILDING!PERMIT!RESIDENTIAL!PLUMBING.js
@@ -19,6 +19,18 @@ if (AInfo["Nature of Work"] == "New" && AInfo["Type of Building"] == "Industrial
 addFee("INDUSTRIAL","CC-BLD-RES-PLUMB","FINAL",1,"Y");
 updateFee("STATELEVY","CC-BLD-RES-PLUMB","FINAL",1,"Y")}
 
+if (AInfo["Nature of Work"] == "New House" && AInfo["Type of Building"] == "Single-Family Dwelling" && !feeExists("NEWSFD")){
+addFee("NEWSFD","CC-BLD-RES-PLUMB","FINAL",1,"Y");
+updateFee("STATELEVY","CC-BLD-RES-PLUMB","FINAL",1,"Y")}
+
+if (AInfo["Nature of Work"] == "New House" && AInfo["Type of Building"] == "Mobile or Manufactured Home on Private Property" && !feeExists("MANUFACTURED")){
+addFee("MANUFACTURED","CC-BLD-RES-PLUMB","FINAL",1,"Y");
+updateFee("STATELEVY","CC-BLD-RES-PLUMB","FINAL",1,"Y")}
+
+if (AInfo["Nature of Work"] == "New House" && AInfo["Type of Building"] == "Industrialized (Modular Home)" && !feeExists("INDUSTRIAL")){
+addFee("INDUSTRIAL","CC-BLD-RES-PLUMB","FINAL",1,"Y");
+updateFee("STATELEVY","CC-BLD-RES-PLUMB","FINAL",1,"Y")}
+
 if (AInfo["Nature of Work"] == "Addition, Renovation, Conversion" && AInfo["Rough-in Inspection"] == "Not Required" && !feeExists("EXISTROUGH")){
 addFee("EXISTROUGH","CC-BLD-RES-PLUMB","FINAL",1,"Y");
 updateFee("STATELEVY","CC-BLD-RES-PLUMB","FINAL",1,"Y")}
@@ -32,5 +44,13 @@ addFee("MULTIFAMILY","CC-BLD-RES-PLUMB","FINAL",1,"Y");
 updateFee("STATELEVY","CC-BLD-RES-PLUMB","FINAL",1,"Y")}
 
 if (AInfo["Nature of Work"] == "New" && AInfo["Type of Building"] == "Accessory Structure" && !feeExists("ROUGHIN")){
+addFee("ROUGHIN","CC-BLD-RES-PLUMB","FINAL",1,"Y");
+updateFee("STATELEVY","CC-BLD-RES-PLUMB","FINAL",1,"Y")}
+
+if (AInfo["Nature of Work"] == "New House" && AInfo["Type of Building"] == "Multi-Family Dwelling" && !feeExists("MULTIFAMILY")){
+addFee("MULTIFAMILY","CC-BLD-RES-PLUMB","FINAL",1,"Y");
+updateFee("STATELEVY","CC-BLD-RES-PLUMB","FINAL",1,"Y")}
+
+if (AInfo["Nature of Work"] == "New House" && AInfo["Type of Building"] == "Accessory Structure" && !feeExists("ROUGHIN")){
 addFee("ROUGHIN","CC-BLD-RES-PLUMB","FINAL",1,"Y");
 updateFee("STATELEVY","CC-BLD-RES-PLUMB","FINAL",1,"Y")}
