be4a3ebeaf87dc0e1bd46acd6e032a6cef280f15
David Boucher <66446858+bushatos@users.noreply.github.com>
2021-03-30 06:23:18 -0400

    added email for amendment

--- Scripts/Event/WTUA;BUILDING!PERMIT!~!~.js
+++ Scripts/Event/WTUA;BUILDING!PERMIT!~!~.js
@@ -177,6 +177,36 @@ try {
 			sendNotification(emailSendFrom, emailSendTo, emailCC, emailTemplate, emailParameters, fileNames);
 		} else { logDebug("No applicants for " + capIDString); }
 	}
+	if (wfTask == "Inspections" && wfStatus == "Amendment Issued") { 
+		var emailSendFrom = '';
+		var emailSendTo = "";
+		var emailCC = "";
+		var emailTemplate = "WTUA_CONTACT NOTIFICATION_AMEND_BLD";
+		var fileNames = [];
+		var emailParameters = aa.util.newHashtable();
+		getRecordParams4Notification(emailParameters);
+		getAPOParams4Notification(emailParameters);
+		var acaSite = lookup("ACA_CONFIGS", "ACA_SITE");
+		acaSite = acaSite.substr(0, acaSite.toUpperCase().indexOf("/ADMIN"));
+		//getACARecordParam4Notification(emailParameters,acaSite);
+		addParameter(emailParameters, "$$acaRecordUrl$$", getACARecordURL(acaSite));
+		addParameter(emailParameters, "$$wfComment$$", wfComment);
+		addParameter(emailParameters, "$$wfStatus$$", wfStatus);
+		addParameter(emailParameters, "$$ShortNotes$$", getShortNotes());
+		var applicantEmail = "";
+		var contObj = {};
+		contObj = getContactArray(capId);
+		if (typeof(contObj) == "object") {
+			for (co in contObj) {
+				if (contObj[co]["contactType"] == "Applicant" && contObj[co]["email"] != null)
+					applicantEmail += contObj[co]["email"] + ";";
+			}
+			addParameter(emailParameters, "$$applicantEmail$$", applicantEmail);
+		} else { logDebug("No contacts at all for " + capIDString); }
+		if (applicantEmail != "") {
+			sendNotification(emailSendFrom, emailSendTo, emailCC, emailTemplate, emailParameters, fileNames);
+		} else { logDebug("No applicants for " + capIDString); }
+	}
 } catch (err) {
 	logDebug("A JavaScript Error occurred: " + err.message + " In Line " + err.lineNumber + " of " + err.fileName + " Stack " + err.stack);
 }
